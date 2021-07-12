ff40a279003858b0a9e451819be815655cb07f6c
XiaoLinGuan <xiaolinggguan@gmail.com>
2021-07-12 17:47:18 -0400

    R codes used to genarate a random sample from the original dataset

---
+++ P3/pro3.r
@@ -0,0 +1,12 @@
+library(dplyr)
+
+data <- read.csv("C:/Users/xiaol/OneDrive/Summer2021/CSCI39579_Data_Visualization/Project/NYC_Dog_Licensing_Data.csv")
+print(ncol(data))
+print(nrow(data))
+View(data)
+
+randomSample <- data %>% sample_n(5000)
+print(ncol(randomSample))
+print(nrow(randomSample))
+View(randomSample)
+write.csv(randomSample, "randomSample.csv", row.names = FALSE)
